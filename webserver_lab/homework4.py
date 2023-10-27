cddee30dd9a7ab2f84e3399f0a58760306c964aa
Eddie Kwak <65310217+Eddiek1102@users.noreply.github.com>
2023-10-26 21:28:38 -0400

    Add files via upload

---
+++ webserver_lab/homework4.py
@@ -0,0 +1,50 @@
+from socket import * 
+import sys 
+
+##### http://192.168.1.155:6770/HelloWorld.html 
+##### 
+
+serverSocket = socket(AF_INET, SOCK_STREAM) 
+
+#Prepare a server socket 
+#Fill in start 
+serverPort = 6770
+serverSocket.bind(('', serverPort)) 
+serverSocket.listen(1) 
+#Fill in end 
+
+while True: 
+    #Establish the connection 
+    print('Ready to serve...') 
+    connectionSocket, addr = serverSocket.accept() 
+    
+    try: 
+        message = connectionSocket.recv(1024).decode() 
+        filename = message.split()[1] 
+        f = open(filename[1:])
+        outputdata = f.read() 
+        
+        #Fill in start 
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        #Fill in end 
+        
+        for i in range(0, len(outputdata)): 
+            connectionSocket.send(outputdata[i].encode()) 
+        connectionSocket.send("\r\n".encode()) 
+        connectionSocket.close() 
+    
+    except IOError: 
+        #Fill in start
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode())
+        #Fill in end 
+        
+        #Close client socket 
+        #Fill in start 
+        connectionSocket.close() 
+        #Fill in end 
+        
+    serverSocket.close() 
+    sys.exit() 
+    
+
